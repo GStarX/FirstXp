a8444f8e64e5b354d16e95ae3cb3d588d52b22d8
XuanRanDev <3135419729@qq.com>
2021-05-11 11:18:01 +0800

    first commit

--- app/src/main/java/com/XuanRan/FirstXp/Main.java
+++ app/src/main/java/com/XuanRan/FirstXp/Main.java
@@ -2,16 +2,38 @@ package com.XuanRan.FirstXp;
 
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.*;
+import android.widget.TextView;
+import de.robv.android.xposed.XC_MethodHook.MethodHookParam;
 
+/**
+ *   Created By XuanRan on 2021/05/11
+ *   演示Xposed开发
+ */
 
 public class Main implements IXposedHookLoadPackage
 {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
     {
-        if(loadPackageParam.packageName.equals("")){
-            Class<?> clazz=XposedHelpers.findClass("",loadPackageParam.classLoader);
-            
+		//指定要Hook的应用
+		
+        if(loadPackageParam.packageName.equals("com.XuanRan.FirstXp")){
+			
+			XposedBridge.hookAllMethods(TextView.class,"setText",new ChangeText());
+			
         }
     }
+}
+
+
+class ChangeText extends XC_MethodHook
+{
+
+	@Override
+	protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+	{
+		super.beforeHookedMethod(param);
+		param.args[0] = "卧槽";
+	}
+	
 }
